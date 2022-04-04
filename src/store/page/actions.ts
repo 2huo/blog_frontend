0475ad4d8af00e308007dab92f215d7fbdb5a592
2huo <maipianlaoxu@gmail.com>
2022-04-04 17:13:35 +0800

    修改复杂操作到action中

--- src/store/page/actions.ts
+++ src/store/page/actions.ts
@@ -1,7 +1,21 @@
 import * as TYPES from '../types';
 import { Dispatch } from 'redux';
+import { getClientWidth } from '@/utils';
 
-export function setSiderStatus(params: { isShow: boolean; sideContent?: JSX.Element | null }) {
+export function setSiderStatus(params: {
+  isShow: boolean;
+  sideContent?: JSX.Element | null;
+}): (Dispatch) => void {
+  const SM = 640;
+  // 判断是否是手机 宽度大于640
+  const width = getClientWidth();
+  // console.log(width);
+  if (params.isShow && width && width >= SM) {
+    // 自由在宽度大于640时才显示sider
+    params.isShow = true;
+  } else {
+    params.isShow = false;
+  }
   return (dispatch: Dispatch): void => {
     dispatch({
       type: TYPES.SIDER_STATUS,
@@ -10,7 +24,7 @@ export function setSiderStatus(params: { isShow: boolean; sideContent?: JSX.Elem
   };
 }
 
-export function setTitle(params: { title: string }) {
+export function setTitle(params: { title: string }): (Dispatch) => void {
   return (dispatch: Dispatch) => {
     dispatch({
       type: TYPES.PAGE_TITLE,
--- src/store/page/reducer.ts
+++ src/store/page/reducer.ts
@@ -1,5 +1,4 @@
 import * as TYPES from '../types';
-import { getClientWidth } from '@/utils';
 
 const defaultState = {
   isShow: false,
@@ -15,19 +14,7 @@ interface IAction {
 export default function SideReducer(state = defaultState, action: IAction): typeof state {
   const { type, payload } = action;
   switch (type) {
-    case TYPES.SIDER_STATUS: {
-      const SM = 640;
-      // 判断是否是手机 宽度大于640
-      const width = getClientWidth();
-      // console.log(width);
-      if (payload.isShow && width && width >= SM) {
-        // 自由在宽度大于640时才显示sider
-        payload.isShow = true;
-      } else {
-        payload.isShow = false;
-      }
-      return { ...state, ...payload };
-    }
+    case TYPES.SIDER_STATUS:
     case TYPES.PAGE_TITLE: {
       return { ...state, ...payload };
     }
