e81e91c672cd1c13ad4d9f9eb4ebbe372d9f3cba
2huo <maipianlaoxu@gmail.com>
2022-08-07 16:59:55 +0800

    style: 修改首页

--- src/components/web/Home/index.tsx
+++ src/components/web/Home/index.tsx
@@ -24,10 +24,10 @@ function Home(): JSX.Element {
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className=" text-xl sm:text-3xl text-black mt-10 flex items-center">
-        <span className="mr-4">愿你我都能保持对前端的热爱</span>
+        <span className="mr-4">你好👋，朋友</span>
         <SmileOutlined />
       </h2>
-      <h2 className="text-xl sm:text-3xl text-gray-400 mt-5">Keep the love for the front-end</h2>
+      <h2 className="text-xl sm:text-3xl text-gray-400 mt-5">Hello, My friend</h2>
       <div className="links">
         <a href={userConfig.github} target="_blank" rel="noopener noreferrer">
           <GithubOutlined />
